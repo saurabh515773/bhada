2ffacfddb6841cefa852bdd1f9dca4e72b4bd1c3
saurabh515773 <awasthi.saurabh91@gmai.com>
2022-01-04 17:27:04 +0530

    base code

---
+++ src/main/java/com/bhada/BhadaApplication.java
@@ -0,0 +1,14 @@
+package com.bhada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhadaApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Bhada!!");
+		SpringApplication.run(BhadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhada/BhadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BhadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
